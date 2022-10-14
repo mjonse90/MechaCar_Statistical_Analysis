af6a597211d5cd5e20c1a38ba979430d31c38c47
Mark Jonse <103767830+mjonse90@users.noreply.github.com>
2022-10-13 21:57:43 -0500

    Add files via upload
    Added Deliverable 2

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -7,3 +7,20 @@ mechaCarMpgDF <- read.csv('/Users/markjonse90/Desktop/Classwork/R_Analysis/mecha
 model<-lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = mechaCarMpgDF)
 
 summary(model)
+
+# Deliverable 2
+
+library(dplyr)
+
+mechaCarSuspensionCoilDF <- read.csv('/Users/markjonse90/Desktop/Classwork/R_Analysis/suspension_coil.csv', check.names = F, stringsAsFactors = F)
+
+total_summary <- mechaCarSuspensionCoilDF %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups = 'keep')
+
+total_summary
+
+lot_summary <- mechaCarSuspensionCoilDF %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups = 'keep') %>% as.data.frame
+
+lot_summary
+
+
+
